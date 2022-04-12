112732646027445dba2daac12cd6c96a7aab712a
harakanvarvas <70520209+harakanvarvas@users.noreply.github.com>
2022-04-12 18:36:17 +0300

    päivitetty siistimpään muotoon

--- harjoitustyo/src/calculator.py
+++ harjoitustyo/src/calculator.py
@@ -4,21 +4,14 @@ class Calculator:
 
     def calculate(amount, frequency, last):
 
+        result = 0
+
         if int(last) == 0:
             result = int(amount) * int(frequency)
-        elif int(amount) == 0:
-            result = 0
-        elif int(frequency) == 0:
-            result = 0
-        elif int(amount) < 0:
-            return False
-        elif int(frequency) < 0:
-            return = False
-        elif int(last) < 0:
+        elif int(amount) < 0 or int(frequency) < 0 or int(last) < 0:
             return False
         elif int(amount) == 1:
             return f"Aikuistuminen tapahtuu arviolta {int(last)} päivän kuluttua"
-
 
         else:
             result = (int(amount) - 1) * int(frequency) + int(last)
@@ -27,7 +20,7 @@ class Calculator:
             result = result // 7
             return f"Aikuistuminen tapahtuu arviolta {result} viikon kuluttua"
 
-        elif result <= 7:
+        if result <= 7:
             return f"Aikuistuminen tapahtuu arviolta {result} päivän kuluttua"
 
         else:
