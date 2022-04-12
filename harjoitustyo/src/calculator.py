969e549f8c963abc3029d5cf3b9c585fc62d9014
harakanvarvas <70520209+harakanvarvas@users.noreply.github.com>
2022-04-12 21:08:43 +0300

    korjauksia

--- harjoitustyo/src/calculator.py
+++ harjoitustyo/src/calculator.py
@@ -1,4 +1,4 @@
-
+from calendar import Calendar
 
 class Calculator:
 
@@ -11,17 +11,13 @@ class Calculator:
         elif int(amount) < 0 or int(frequency) < 0 or int(last) < 0:
             return False
         elif int(amount) == 1:
-            return f"Aikuistuminen tapahtuu arviolta {int(last)} päivän kuluttua"
-
+            return int(last)
         else:
             result = (int(amount) - 1) * int(frequency) + int(last)
 
-        if result > 7:
-            result = result // 7
-            return f"Aikuistuminen tapahtuu arviolta {result} viikon kuluttua"
+        return result
 
-        if result <= 7:
-            return f"Aikuistuminen tapahtuu arviolta {result} päivän kuluttua"
-
-        else:
-            return result
+    def weeks(days):
+        weeks = days // 7
+        maturing_week = Calendar.maturing_week(days)
+        return (weeks, maturing_week)
